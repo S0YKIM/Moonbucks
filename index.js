fb119d1390f60dca0775f3c4ff529b131f51e4c3
S0YKIM <soyeonkim.9683@gmail.com>
2021-09-23 21:25:14 +0900

    step2

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-// TODO 메뉴 추가
-// [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
-// [x] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 추가한다.
-// [x] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
-// [x] 총 메뉴 개수를 세서 상단에 보여준다.
-// [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-// [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+//  TODO localStorage Read & Write
+//  [] localStorage 에 데이터를 저장한다.
+//  [] 새로고침해도 데이터가 남아있다.
 
-// TODO 메뉴 수정
-// [x] 메뉴의 수정버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
-// [x] 모달창에서 메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.
+//  TODO 카테고리별 메뉴판 관리
+//  [] 에스프레소 메뉴판 관리
+//  [] 프라푸치노 메뉴판 관리
+//  [] 블렌디드 메뉴판 관리
+//  [] 티바나 메뉴판 관리
+//  [] 디저트 메뉴판 관리
 
-// TODO 메뉴 삭제
-// [x] 메뉴의 삭제버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌(confirm) 모달창이 뜬다.
-// [x] 확인 버튼을 클릭하면 메뉴가 삭제된다.
-// [x] 총 메뉴 개수를 count 하여 상단에 보여준다.
+//  TODO 페이지 접근시 최초 데이터 Read & Rendering
+//  [] 페이지를 최초로 로딩할 때 localStorage에 에스프레소 메뉴를 읽어온다.
+//  [] 에스프레소 메뉴를 페이지에 그려준다.
+
+//  TODO 품절 상태 관리
+//  [] 품절 버튼을 추가한다.
+//  [] 품절 버튼을 클릭하면 localStorage 에 상태값이 저장된다.
+//  [] 클릭 이벤트에서 가장 가까운 li 태그의 class 속성 값에 sold-out 을 추가한다.
 
 const $ = (selector) => document.querySelector(selector);
 
+const store = {
+    setLocalStorage(menu) {
+        localStorage.setItem("menu", JSON.stringify(menu));
+    },
+    getLocalStorage() {
+        localStorage.getItem("menu");
+    },
+}
 function App() {
+    // 상태: 메뉴명
+    this.menu = [];
+
     // 총 메뉴 개수를 세는 함수
     const updateMenuCount = () => {
         const menuCount = $("#espresso-menu-list"). querySelectorAll("li").length;
@@ -31,10 +45,12 @@ function App() {
             return;
         }
         const espressoMenuName = $("#espresso-menu-name").value;
-        const menuItemTemplate = (espressoMenuName) => {
+        this.menu.push({ name: espressoMenuName });
+        store.setLocalStorage(this.menu);
+        const template = this.menu.map((item) => {
             return `
                 <li class="menu-list-item d-flex items-center py-2">
-                    <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+                    <span class="w-100 pl-2 menu-name">${item.name}</span>
                     <button
                         type="button"
                         class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -47,11 +63,11 @@ function App() {
                 >
                     삭제
                 </button>
-            </li>`
-        };
-        $("#espresso-menu-list").insertAdjacentHTML(
-            "beforeend", menuItemTemplate(espressoMenuName)
-        );
+            </li>`;
+        })
+        .join("");
+
+        $("#espresso-menu-list").innerHTML = template;
         updateMenuCount();
         $("#espresso-menu-name").value = "";
     };
